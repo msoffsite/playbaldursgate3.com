725f556393824f9c8ef1569e0646161db86f4a4b
msoffsite <mark@offsitedeveloper.com>
2024-04-14 13:22:01 -0400

    Added area for mods and page for learning about and downloading level cap extender.

---
+++ Areas/Mods/Pages/Level-Cap-Extender.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlayBaldursGate3.Areas.Mods.Pages
+{
+    public class Level_Cap_ExtenderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
